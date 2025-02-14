7560499405e27307794b3e4605dfcd6a4bc396dd
Heypon <93323799+necoleo@users.noreply.github.com>
2025-02-14 15:01:44 +0800

    Add files via upload
    本地命令行代码生成器

---
+++ Heypon-CodeGenerator/src/main/java/com/Heypon/model/MainTemplateModel.java
@@ -0,0 +1,27 @@
+package com.Heypon.model;
+
+import lombok.Data;
+
+@Data
+public class MainTemplateModel {
+    /*
+        1. 在代码开头增加作者 @Author 注释 （增加代码）
+        2. 修改程序输出的信息提示 （替换代码）
+        3. 将循环读取输入改为单次读取 （可选代码）
+    */
+
+    /**
+     * 作者名称
+     */
+    private String author = "Heypon";
+
+    /**
+     * 输出信息
+     */
+    private String outputText = "sum";
+
+    /**
+     * 是否循环
+     */
+    private boolean loop;
+}
